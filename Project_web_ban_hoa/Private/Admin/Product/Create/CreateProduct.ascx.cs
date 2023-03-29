7551022422b0b355c35188af562630066936e1a5
TaiLe1101 <taic21a.th1@gmail.com>
2023-03-29 20:40:41 +0700

    update: fix bug image

--- Project_web_ban_hoa/Private/Admin/Product/Create/CreateProduct.ascx.cs
+++ Project_web_ban_hoa/Private/Admin/Product/Create/CreateProduct.ascx.cs
@@ -40,31 +40,43 @@ namespace Project_web_ban_hoa.Private.Admin.Product.Create
             double currentPrice = Convert.ToDouble(txtCurrentPrice.Text);
             int categoryId = Convert.ToInt32(ddlCategory.SelectedValue);
             int quantity = Convert.ToInt32(txtQuantiry.Text);
+            string thumbnail;
 
             if (fulThumbnail.HasFile)
             {
-                HttpPostedFile file = Request.Files[0];
-                if (file.ContentType.ToLower().StartsWith("image/"))
+                if (fulThumbnail.PostedFile.ContentType == "image/")
                 {
-                    string fileName = Path.GetFileName(file.FileName).Replace(" ", "-");
+                    string fileName = Path.GetFileName(fulThumbnail.FileName).Replace(" ", "-");
                     string saveFileName = Components.ConvertToUnSign(fileName);
-                    string savePath = Server.MapPath("~/Publics/Uploads/Category/" + saveFileName);
-                    string thumbnail = ConfigurationManager.AppSettings["UrlEnv"] + $"/Publics/Uploads/Category/{saveFileName}";
-                    int n = DAO.Product.CreateProduct(nameProduct, seoName, descProduct, oldPrice, currentPrice, quantity, thumbnail, categoryId);
-                    if (n > 0)
-                    {
-                        fulThumbnail.SaveAs(savePath);
-                        Session["showToastMessage"] = "Tạo sản phẩm thành công";
-                        Session["showToastDuration"] = 3000;
-                        Session["showToastPosition"] = "right";
-                        Response.Redirect("~/Admin.aspx?modul=product&sub-modul=view-products");
-                    }
+                    string savePath = Server.MapPath("~/Publics/Uploads/Product/" + saveFileName);
+                    fulThumbnail.SaveAs(savePath);
+                    thumbnail = ConfigurationManager.AppSettings["UrlEnv"] + $"/Publics/Uploads/Product/{saveFileName}";// https://localhost:113/Publics/Uploads/Product/a-1.jpg
                 }
                 else
                 {
-
+                    string message = "Hình ảnh không hợp lệ";
+                    int duration = 3000;
+                    string position = "right";
+                    string backColor = "red";
+                    string script = "showToast('" + message + "', " + duration + ", '" + position + "', '" + backColor + "')";
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "ShowToast", script, true);
+                    return;
                 }
             }
+            else
+            {
+                thumbnail = ConfigurationManager.AppSettings["UrlEnv"] + $"/Publics/Images/DefaultImg/no-image.jpg";
+            }
+
+            int n = DAO.Product.CreateProduct(nameProduct, seoName, descProduct, oldPrice, currentPrice, quantity, thumbnail, categoryId);
+            if (n > 0)
+            {
+                Session["showToastMessage"] = "Tạo sản phẩm thành công";
+                Session["showToastDuration"] = 3000;
+                Session["showToastPosition"] = "right";
+                Response.Redirect("~/Admin.aspx?modul=product&sub-modul=view-products");
+            }
+
         }
     }
 }
