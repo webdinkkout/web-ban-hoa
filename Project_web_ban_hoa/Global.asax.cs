6579480552b5bdf0263df06684ae383b64e8fbb0
TaiLe1101 <taic21a.th1@gmail.com>
2023-04-02 18:46:53 +0700

    update: delete cache

--- Project_web_ban_hoa/Global.asax.cs
+++ Project_web_ban_hoa/Global.asax.cs
@@ -15,7 +15,6 @@ namespace Project_web_ban_hoa
         protected void Application_Start(object sender, EventArgs e)
         {
             DAO.SqlDatabase.ConnectString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
-            RouteTable.Routes.MapPageRoute("LoginRoute", "Login/{slug}", "~/Login.aspx", false);
         }
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
