01ef40f524efb8490b91e5009f2a39c7b5f5bb26
tantai0610 <tainguyen9147@gmail.com>
2023-04-03 00:36:29 +0700

    update:  cònlig

--- Project_web_ban_hoa/Layout/Main.Master.cs
+++ Project_web_ban_hoa/Layout/Main.Master.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,20 @@ namespace Project_web_ban_hoa.Layout
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                rptCategoriesNav.DataSource = DAO.Category.GetAllCategories(1, 9, 0);
 
+                rptCategoriesNav.DataBind();
+
+            }
+
+
+        }
+
+        protected DataTable LayConThuocCha(int parentId)
+        {
+            return DAO.Category.GetCategoryByParentIdAndLevel(parentId);
         }
     }
 }
--- Project_web_ban_hoa/Layout/Main.Master.designer.cs
+++ Project_web_ban_hoa/Layout/Main.Master.designer.cs
@@ -33,6 +33,15 @@ namespace Project_web_ban_hoa.Layout
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// rptCategoriesNav control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptCategoriesNav;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
