79b1de003a074e7f07e63d586457529ef6b0d7df
Dung1233123123 <trannhatdung249@gmail.com>
2023-04-04 16:42:43 +0700

    update: render categories

--- Project_web_ban_hoa/Product.aspx.designer.cs
+++ Project_web_ban_hoa/Product.aspx.designer.cs
@@ -22,14 +22,5 @@ namespace Project_web_ban_hoa
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater rptCategories;
-
-        /// <summary>
-        /// rptRenderCard control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater rptRenderCard;
     }
 }
