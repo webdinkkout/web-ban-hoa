ace37ce6c4e1e78c7d28ac5bbd7c4eb64a73186d
TaiLe1101 <taic21a.th1@gmail.com>
2023-04-05 20:21:03 +0700

    update: href

--- Project_web_ban_hoa/Models/CategoryModel.cs
+++ Project_web_ban_hoa/Models/CategoryModel.cs
@@ -12,5 +12,6 @@ namespace Project_web_ban_hoa.Models
         public string SeoName { get; set; }
         public string Thumbnail { get; set; }
         public int? ParentID { get; set; }
+        public int Level { get; set; }
     }
 }
--- Project_web_ban_hoa/Product.aspx.cs
+++ Project_web_ban_hoa/Product.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_web_ban_hoa.Models;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -10,17 +11,45 @@ namespace Project_web_ban_hoa
 {
     public partial class Product1 : System.Web.UI.Page
     {
+        int parentId;
+        CategoryModel category = new CategoryModel();
+
         protected DataTable GetProductID(int id)
         {
             return DAO.Product.GetProductWithCategoryId(id);
         }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack)
             {
-                rptCategories.DataSource = DAO.Category.GetCategoryByParentIdAndLevel(9);
+                if (Request.QueryString["pi"] != null)
+                    parentId = Convert.ToInt32(Request.QueryString["pi"]);
+
+                rptCategories.DataSource = DAO.Category.GetCategoryByParentIdAndLevel(parentId);
                 rptCategories.DataBind();
+
+                rptMenuSubCategories.DataSource = DAO.Category.GetCategoryByParentIdAndLevel(parentId);
+                rptMenuSubCategories.DataBind();
+
+                DataTable categories = DAO.Category.GetOneCategory(parentId);
+                if (categories != null)
+                {
+                    DataRow rCategry = categories.Rows[0];
+
+                    category.Id = Convert.ToInt32(rCategry["Id"].ToString());
+                    category.Name = rCategry["Name"].ToString();
+                    category.SeoName = rCategry["Seo_Name"].ToString();
+                    category.ParentID = rCategry["Parent_Id"] as int?;
+                    category.Thumbnail = rCategry["Thumbnail"].ToString();
+                    category.Level = Convert.ToInt32(rCategry["Level"].ToString());
+                }
             }
+        }
+
+        protected string GetNameCategory()
+        {
+            return category.Name;
         }
     }
 }
